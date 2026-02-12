6d45f523155917392c01759cf4256aab6aeb02a5
Arshnoor Singh <arshnoor1607@gmail.com>
2026-02-12 12:23:05 +0530

    Feature:Added logic

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const container = document.getElementById('container')
 const input = document.getElementById("weight")
 const btn = document.getElementById("btn")
+const ans=document.getElementById("ans1")
 
 btn.addEventListener('click', () => {
     const InputValue = input.value;
     let convertedValue=(InputValue*0.453592);
-    let Output=document.createElement("span");
-    Output.id="output"
-    Output.style.display="inline-block"
-    Outp
+    // window.alert(convertedValue.toFixed(2)+"Kg")
+    ans.innerText+=` ${convertedValue.toFixed(2)}Kg`
+    
+
+    // Output.style.display="inline-block"
+    // Outp
 
 })
 
