d4d87eb490a0061122b1e416082b2d9103609a07
Arshnoor Singh <arshnoor1607@gmail.com>
2026-02-12 11:31:17 +0530

    Added basic layout

---
+++ app.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('container')
+const input = document.getElementById("weight")
+const btn = document.getElementById("btn")
+
+btn.addEventListener('click', () => {
+    const InputValue = input.value;
+    let convertedValue=(InputValue*0.453592);
+    let Output=document.createElement("span");
+    Output.id="output"
+    Output.style.display="inline-block"
+    Outp
+
+})
+
